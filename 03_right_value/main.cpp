df7682a15084722dc0d29409dfef603b32a7b66d
v138468 <v138468@gmail.com>
2022-09-22 16:26:09 +0800

    init

--- 03_right_value/main.cpp
+++ 03_right_value/main.cpp
@@ -1,32 +1,24 @@
-#include <iostream>
+#include <type_traits>
 #include <vector>
-#include <algorithm>
+std::vector<int> foo()
+{
+    std::vector<int> temp = {1, 2, 3, 4};
+    return temp;
+}
 
 int main()
 {
-    std::vector<int> vec = {1, 2, 3, 4};
+    // 正确，"01234" 类型为 const char [6]，因此是左值
+    const char(&left)[6] = "01234";
 
-    // 在 c++17 之前
-    const std::vector<int>::iterator itr = std::find(vec.begin(), vec.end(), 2);
-    if (itr != vec.end())
-    {
-        *itr = 3;
-    }
+    // 断言正确，确实是 const char [6] 类型，注意 decltype(expr) 在 expr 是左值
+    // 且非无括号包裹的 id 表达式与类成员表达式时，会返回左值引用
+    static_assert(std::is_same<decltype("01234"), const char(&)[6]>::value, "");
 
-    // 需要重新定义一个新的变量
-    const std::vector<int>::iterator itr2 = std::find(vec.begin(), vec.end(), 3);
-    if (itr2 != vec.end())
-    {
-        *itr2 = 4;
-    }
-
-    // 将临时变量放到 if 语句内
-    if (const std::vector<int>::iterator itr = std::find(vec.begin(), vec.end(), 3); itr != vec.end())
-    {
-        *itr = 4;
-    }
-
-    // 将输出 1, 4, 3, 4
-    for (std::vector<int>::iterator element = vec.begin(); element != vec.end(); ++element)
-        std::cout << *element << std::endl;
+    const char *p = "01234";    // 正确，"01234" 被隐式转换为 const char*
+    const char *&&pr = "01234"; // 正确，"01234" 被隐式转换为 const char*，该转换的结果是纯右值
+    // const char*& pl = "01234"; // 错误，此处不存在 const char* 类型的左值
+    std::vector<int> v = foo();
+    // 错误，"01234" 是左值，不可被右值引用
+    // const char (&&right)[6] = "01234";
 }
